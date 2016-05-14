fa673690326eb03295037e48bc7add91c3e36625
luoyiqing <sarahslucky@gmail.com>
2016-05-14 14:00:27 -0400

    implmented import function in PKFernet

--- PKFernet.py
+++ PKFernet.py
@@ -19,7 +19,12 @@ class InvalidToken(Exception):
     pass
 
 class PKFernet(object):
+
 	def __init__(self, private_keyring, public_keyring, backend=None):
+		# empty dictionary for receivers and their public key rings
+
+		self.recerver_dict = {}
+
 		if backend is None:
 			backend = default_backend()
 
@@ -36,8 +41,8 @@ class PKFernet(object):
 		self._backend = backend
 
 	def extract_priv_key(self, private_keyring):
-		print(private_keyring.split(":" and ",", 1))
-		
+		return (private_keyring.split(":" and ",", 1))
+
 
 	def extract_pub_key(self, public_keyring):
 		pass
@@ -45,14 +50,23 @@ class PKFernet(object):
 	@classmethod
 	def generate_key(cls):
 		return base64.urlsafe_b64encode(os.urandom(32))
-		
+
 	def encrypt(self, msg, receiver_name, receiver_enc_pub_key_alias, sender_sign_header, adata = '', sign_also=True):
 		pass
 	def decrypt(self, ctx, sender_name, verfiy_also=True):
 		pass
 	# my_pub_keys_json_blob = pf.export_pub_keys(key_alias_list=[])
 	def export_pub_keys(self, key_alias_list=[]):
+
 		pass
 	# pf.import_pub_keys(receiver_name, receiver_public_keyring)
 	def import_pub_keys(self, receiver_name, receiver_public_keyring):
+		if receiver_name in self.recerver_dict.keys():
+			for key in receiver_public_keyring:
+				self.recerver_dict[receiver_name].add(key)
+		else:
+			self.recerver_dict[receiver_name] = {}
+			for key in receiver_public_keyring:
+				self.recerver_dict[receiver_name].add(key)
+
 		pass
